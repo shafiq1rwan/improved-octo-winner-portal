39637e80ddb61f0bd351fedc3b35a9ac60423a3f
lipsheng.tan <lipsheng.tan@mpsb.net>
2018-11-13 12:18:07 +0800

    Commit project base files.

---
+++ src/main/java/managepay/com/admin/byod/ByodApplication.java
@@ -0,0 +1,12 @@
+package managepay.com.admin.byod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByodApplication.class, args);
+	}
+}
